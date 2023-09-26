4138959080b04afe30e0b1434a787513ad6c6b7a
Pratheek Nayak B K <128373632+NayakPratheek@users.noreply.github.com>
2023-09-26 14:49:36 +0530

    data.js

---
+++ data.js
@@ -0,0 +1,62 @@
+let songs = [
+    {
+    name: 'Slow Dance in the Dark',
+    path:'songs/slow.mp3',
+    artist: 'Joji',
+    cover: 'images/slow dance.jpg'
+    },
+    {
+        name: 'Glimpse of Us',
+        path:'songs/glimpse.mp3',
+        artist:'Joji',
+        cover:'images/glimpse.jpg'
+    },
+    {
+        name: 'Lovely',
+        path:'songs/lovely.mp3',
+        artist:'Billie Eilish, Khalid',
+        cover:'images/lovely.png'
+    },
+    {
+        name: 'I Wanna be Yours',
+        path:'songs/wanna.mp3',
+        artist:'Arctic Monkeys',
+        cover:'images/wanna.jpg'
+    },
+    {
+        name: 'Die For You',
+        path:'songs/die.mp3',
+        artist:'The Weeknd',
+        cover:'images/die.jpg'
+    },
+    {
+        name: 'Here With Me',
+        path:'songs/here.mp3',
+        artist:'CHVRCHES, Marshmello',
+        cover:'images/here.jpg'
+    },
+    {
+        name: 'Dandelions',
+        path:'songs/dand.mp3',
+        artist:'Ruth B',
+        cover:'images/dand.jpg'
+    },
+    {
+        name: 'Golden Hour',
+        path:'songs/golden.mp3',
+        artist:'JVKE',
+        cover:'images/here.jpg'
+    },
+    {
+        name: 'Escapism',
+        path:'songs/escape.mp3',
+        artist:'RAYE, 070 Shake',
+        cover:'images/escape.png'
+    },
+    {
+        name: 'Lovefool',
+        path:'songs/lovefool.mp3',
+        artist:'twocolors',
+        cover:'images/two color.jpg'
+    },
+];
